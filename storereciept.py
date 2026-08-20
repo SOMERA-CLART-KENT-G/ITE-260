ac3ac03530086de08283adbe9b20797c4f4441a5
SOMERA CLART KENT G. <clgr.somera.au@phinmaed.com>
2026-08-20 19:25:50 +0800

    Add files via upload
    ITE-260 fist activity

---
+++ storereciept.py
@@ -0,0 +1,11 @@
+age = int(input("enter your age"))
+
+if age >= 18:
+    print("lob you jhomar")
+    
+elif age >= 50:
+    print("mapanghi na")
+    
+ else: age >= 10:
+     print("anti pedo")
+ 
